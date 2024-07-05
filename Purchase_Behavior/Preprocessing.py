6f3df89ce1bd94b267b3b0eb9caa3df9d1beab94
ankith <ankith15bhandary@gmail.com>
2024-07-05 08:57:44 +0530

    Preprocessings

--- Purchase_Behavior/Preprocessing.py
+++ Purchase_Behavior/Preprocessing.py
@@ -1,2 +1,9 @@
 import numpy as np 
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+df = pd.read_csv('Purchase_Behavior\purchase_history.csv')
+print(df.head())
+
+oh = OneHotEncoder(drop='first')
+df['Gender']=oh.fit_transform(df[['Gender']])
