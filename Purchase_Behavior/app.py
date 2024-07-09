7449d60bc98c710483d5c55fdc879bd1108684fc
ankith <ankith15bhandary@gmail.com>
2024-07-09 08:30:35 +0530

    streamlit app

--- Purchase_Behavior/app.py
+++ Purchase_Behavior/app.py
@@ -15,3 +15,10 @@ with open ('ohe.pkl','rb') as f:
 # buileding the streamlit app
 
 st.title('Understanding the purchase behaviour')
+
+Age = st.number_input('Age',min_value=0,max_value=100,value=25)
+Salary = st.number_input('Salary',min_value=0,max_value=100000000,value=22000)
+Product_ID = st.selectbox('Product ID',['P01','P02','P03'])
+Price = st.number_input('Price',min_value=1,max_value=10000,value=110)
+Gender = st.selectbox('Gender',['Male','Female'])
+
