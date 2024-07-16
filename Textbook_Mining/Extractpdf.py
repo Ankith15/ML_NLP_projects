28989c2378d47ada9959f88aae6f0953b9b2359d
ankith <ankith15bhandary@gmail.com>
2024-07-17 00:50:14 +0530

    Data chunking

--- Textbook_Mining/Extractpdf.py
+++ Textbook_Mining/Extractpdf.py
@@ -19,3 +19,28 @@ pdf_files = [
 extracted_text = extract_text(pdf_files)
 
 print(extracted_text)
+
+
+import nltk
+from nltk.tokenize import sent_tokenize
+
+def chunk_text(text, chunk_size=100):
+    sentences = sent_tokenize(text)
+    chunks = []
+    current_chunk = []
+    current_length = 0
+    
+    for sentence in sentences:
+        sentence_length = len(sentence.split())
+        if current_length + sentence_length <= chunk_size:
+            current_chunk.append(sentence)
+            current_length += sentence_length
+        else:
+            chunks.append(" ".join(current_chunk))
+            current_chunk = [sentence]
+            current_length = sentence_length
+    
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
+    
+    return chunks
